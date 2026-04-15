3ae3863ae2380cc7fd1b2e70486632f8e2097d57
jay-ouo <111067520+jay-ouo@users.noreply.github.com>
2026-04-15 16:36:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/career')
+def career():
+    return render_template('career.html')
+
+@app.route('/job')
+def job():
+    return render_template('job.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/future')
+def future():
+    return render_template('future.html')
